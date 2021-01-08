346abf653bf540ec233f61bf6ffdb36fd2905fd2
Mark Godwin <mark.godwin@logikcull.com>
2021-01-08 13:23:27 -0700

    Adds multiple states to the session to avoid errant CSRF detection when multiple tabs/windows are open.
    This approach ignores the omniauth.origin field, as it is unused for our purposes.

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -72,7 +72,9 @@ module OmniAuth
                         .merge(pkce_authorize_params)
 
         session["omniauth.pkce.verifier"] = options.pkce_verifier if options.pkce
-        session["omniauth.state"] = params[:state]
+        session["omniauth.states"] ||= []
+        session["omniauth.states"].shift if session["omniauth.states"].size > 50 # Prevent cookie overflow if states pile up.
+        session["omniauth.states"] << params[:state]
 
         params
       end
@@ -83,11 +85,14 @@ module OmniAuth
 
       def callback_phase # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
         error = request.params["error_reason"] || request.params["error"]
+        state = request.params["state"].to_s
+        valid_states = session["omniauth.states"] || []
         if error
           fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
-        elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+        elsif !options.provider_ignores_state && (state.empty? || !valid_states.include?(state))
           fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
         else
+          session['omniauth.states']&.delete(state)
           self.access_token = build_access_token
           self.access_token = access_token.refresh! if access_token.expired?
           super
